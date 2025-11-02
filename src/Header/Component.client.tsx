17457888f44985e5653c8c4f129c9fd1ab40d3d2
quantum-grit <quantum.grit@gmail.com>
2025-11-02 22:36:44 +0200

    removed logo inversion

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -33,7 +33,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="container relative z-20   " {...(theme ? {'data-theme': theme} : {})}>
       <div className="py-8 flex justify-between items-center">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high"/>
         </Link>
         <div className="flex items-center gap-4">
           <HeaderNav data={data} />

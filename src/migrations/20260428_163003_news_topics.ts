e9d952d6ef3ea3113c708a5e136173c9a21b8d99
quantum-grit <91589884+quantum-grit@users.noreply.github.com>
2026-04-30 12:50:00 +0300

    fixed sql statements order in migration

--- src/migrations/20260428_163003_news_topics.ts
+++ src/migrations/20260428_163003_news_topics.ts
@@ -2,12 +2,13 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "news" ALTER COLUMN "topic" SET DATA TYPE text;
+  ALTER TABLE "news" ALTER COLUMN "topic" DROP DEFAULT;
+  ALTER TABLE "news" ALTER COLUMN "topic" DROP NOT NULL;
+  ALTER TABLE "news" ALTER COLUMN "topic" SET DATA TYPE text;
   DROP TYPE "public"."enum_news_topic";
   CREATE TYPE "public"."enum_news_topic" AS ENUM('alerts', 'release-notes');
   ALTER TABLE "news" ALTER COLUMN "topic" SET DATA TYPE "public"."enum_news_topic" USING "topic"::"public"."enum_news_topic";
-  ALTER TABLE "news" ALTER COLUMN "topic" DROP DEFAULT;
-  ALTER TABLE "news" ALTER COLUMN "topic" DROP NOT NULL;`)
+  `)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
